1595fc754c032b39c61d0551c5ba462ccc5fdfda
olyanren <olyanren@gmail.com>
2020-10-16 16:01:26 +0300

    stream api usage

--- rest-api.js
+++ rest-api.js
@@ -5,31 +5,42 @@ AWS.config.update(config.aws_remote_config);
 
 // Create the DynamoDB service object
 const db = new AWS.DynamoDB({apiVersion: 'latest'});
+const key = "f3cbfe15b941bb29a281fc3ef17b29b5";
+const cryptLib = require('@skavinvarnan/cryptlib');
 exports.handler = async (event) => {
-  let type = 'CURRENCY';
+  let type = null;
   if (event !== undefined && event.type != null) type = event.type;
-  var params = {
-    TableName: config.aws_table_name,
-    FilterExpression: 'contains (#Type, :post)',
-    ExpressionAttributeNames: {
-      "#Type": "Type",
-    },
-    ExpressionAttributeValues: {
-      ":post": {S: type}
-    }
-  };
+  let params;
+  if (type == null) {
+    params = {
+      TableName: config.aws_table_name,
+
+    };
+  } else {
+    params = {
+      TableName: config.aws_table_name,
+      FilterExpression: 'contains (#Type, :post)',
+      ExpressionAttributeNames: {
+        "#Type": "Type",
+      },
+      ExpressionAttributeValues: {
+        ":post": {S: type}
+      }
+    };
+  }
+
 
   const awsRequest = await db.scan(params);
   try {
     const result = await awsRequest.promise();
     return {
       statusCode: 200,
-      body: result.Items
+      body:cryptLib.encryptPlainTextWithRandomIV(JSON.stringify(result.Items),key)
     };
   } catch (e) {
     return {
       statusCode: 500,
-      body: e.code+": "+e.message
+      body: e.code + ": " + e.message
     };
   }
 
