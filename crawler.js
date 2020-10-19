3b3fe5181dc17dcd5324dc083f05ae233bbeaa56
olyanren <olyanren@gmail.com>
2020-10-19 08:07:27 +0300

    dynamodb item getirme ok

---
+++ crawler.js
@@ -0,0 +1,29 @@
+const parser=require('node-html-parser');
+var https = require('https');
+
+var options = {
+  host: 'finanswebde.com',
+  path: '/doviz',
+}
+var request = https.request(options, function (res) {
+  var data = '';
+  res.on('data', function (chunk) {
+    data += chunk;
+  });
+  res.on('end', function () {
+    const root = parser.parse(data)
+    let rows=root.querySelector('.portfolio tbody').childNodes;
+    rows.forEach(row => {
+      if(row.childNodes.length===7){
+        let code= row.childNodes[0].querySelector('h3 a').childNodes[0].rawText
+        let buying= row.childNodes[1].querySelector('span span').childNodes[0].rawText
+        let selling= row.childNodes[2].querySelector('span span').childNodes[0].rawText
+        let difference= row.childNodes[3].querySelector('span span').childNodes[0].rawText
+      }
+    });
+  });
+});
+request.on('error', function (e) {
+  console.log(e.message);
+});
+request.end();
